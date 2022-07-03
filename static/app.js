14161d7fc28e3847aaed35cae8c6e931bf327a9b
nitin-j04 <nitinpilania5@gmail.com>
2022-07-03 22:33:05 +0530

    deployment

---
+++ static/app.js
@@ -0,0 +1,92 @@
+
+
+const fileUploader = document.getElementById('file');
+const feedback = document.getElementById('feedback');
+const progressbar = document.getElementById('progress');
+const progressLabel = document.getElementById('progress-label')
+const feedbackval = document.getElementById('feedback')
+const alertModal = document.getElementById('alert-modal');
+const alertExtension = document.getElementById('alert-modal-ext');
+const alertSupport = document.getElementById('alert-modal-support')
+// const modalClose = document.getElementById('close');
+
+const reader = new FileReader();
+
+fileUploader.addEventListener('change', (event) => {
+  const files = event.target.files;
+  console.log(files);
+  const file = files[0];
+  reader.readAsDataURL(file);
+  
+  reader.addEventListener('progress', (event) => {
+    if (event.loaded && event.total) {
+      const percent = (event.loaded / event.total) * 100;
+      progressbar.value = percent;
+      progressLabel.innerHTML = Math.round(percent) + '%';
+      
+      if (percent === 100) {
+        let msg = `<span style="color:#00FF7F;">File <u><b>${file.name}</b></u> has been uploaded successfully.</span>`;
+        feedback.innerHTML = msg;
+
+        setTimeout(function(){
+          alertExtension.classList.remove("d-none");
+          alertSupport.classList.remove("d-none");
+        }, 500); 
+      }
+    }
+
+    
+  });
+});
+
+
+
+
+fileUploader.onchange = () => {
+   // Check if any file is selected.
+  if (fileUploader.files.length > 0) {
+    for (let i = 0; i <= fileUploader.files.length - 1; i++) {
+
+        const fsize = fileUploader.files.item(i).size;
+        const filesize = Math.round((fsize / 1024*1.0));
+        // The size of the file.
+        if (filesize >= 20480) {
+         
+         
+          alertModal.classList.remove("d-none");
+          progressbar.classList.add('d-none');
+          feedback.classList.add('d-none');
+          progressLabel.classList.add('d-none');
+
+          
+          setTimeout(function(){
+            alert('will reload!!');
+            window.location.reload();
+          }, 1000); 
+        } else {
+          // progressbar.classList.remove('d-none')
+            document.getElementById('size').innerHTML = '<b>Size:'
+            + filesize + '</b> KB';
+
+            
+        }
+    }
+  }
+}
+
+
+
+
+
+var loadingDiv = document.getElementById('loading');
+const submitButton = document.getElementById('buttonBootstrap')
+
+function showSpinner() {
+  loadingDiv.style.visibility = 'visible';
+  submitButton.style.color = 'black';
+}
+
+window.onload = () => {
+  console.log('page is fully loaded');
+  loadingDiv.style.visibility = 'hidden';
+};
